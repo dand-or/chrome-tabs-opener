9222a891d2d986f88c949d69641359ea91ee352a
d-ando <moruze.orange@gmail.com>
2020-08-04 23:01:36 +0900

    wip

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -4,6 +4,27 @@ import ToggleButton from './atoms/toggleButton';
 import './reset.scss'
 import './popup.scss';
 
+const containers = [
+  {
+    container_id: 1,
+    container_name: "テスト",
+    tab_items: [1,2]
+  }
+];
+
+const tabs = [
+  {
+    tab_id: 1,
+    url: "https://github.com/dand-or?tab=repositories",
+    title: "Your Repositories"
+  },
+  {
+    tab_id: 2,
+    url: "https://github.com/dand-or",
+    title: "dand-or"
+  },
+];
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const PopUp : React.FunctionComponent = () => (
     <React.Fragment>
@@ -12,10 +33,25 @@ const PopUp : React.FunctionComponent = () => (
       </header>
       <main>
         <ul>
-          <li>
-            <p>test</p>
-            <ToggleButton></ToggleButton>
-          </li>
+          {containers.map((c, i) => {
+            return (
+            <li key={i}>
+              <p>{c.container_id}</p>
+              <p>{c.container_name}</p>
+              <ul>
+                {c.tab_items.map((t, j) => {
+                  const item = tabs.find((tab) => { return tab.tab_id === t });
+                  return (
+                  <li key={j}>
+                    <p>{item?.tab_id}</p>
+                    <a href={item?.url} target='_blank'>{item?.title}</a>
+                  </li>
+                  )
+                })}
+              </ul>
+            </li>
+            )
+          })}
         </ul>
       </main>
     </React.Fragment>
